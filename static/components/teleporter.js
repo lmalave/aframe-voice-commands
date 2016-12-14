99fbea60c66acfc0cb4ba2d9ce6feeaa79fdd342
lmalave <lmalave@hugeinc.com>
2016-12-14 18:00:47 -0500

    adding cylindrical teleport marker

--- static/components/teleporter.js
+++ static/components/teleporter.js
@@ -11,7 +11,8 @@ AFRAME.registerSystem('teleporter', {
 AFRAME.registerComponent('teleporter', {
 
     schema: {
-        character: { type: 'selector' }
+        character: { type: 'selector' },
+        marker: { type: 'selector' }
     },
     init: function () {
         console.log("in teleporter init");
@@ -19,15 +20,29 @@ AFRAME.registerComponent('teleporter', {
         this.el.teleportPoint = "0 0 0";
         this.el.hasTeleportPoint = false;
         this.el.addEventListener('raycaster-intersection', function (evt) {
-            console.log('Raycast hit something!, evt intersection point: '+AFRAME.utils.coordinates.stringify(evt.detail.intersections[0].point));
+           // console.log('Raycast hit something!, evt intersection point: '+AFRAME.utils.coordinates.stringify(evt.detail.intersections[0].point));
             //var teleportSystem = document.querySelector('a-scene').systems['teleporter'];
             //teleportSystem.teleportPosition = "0 0 0";
+            var intersectionPoint = evt.detail.intersections[0].point;
             this.teleportPoint = AFRAME.utils.coordinates.stringify(evt.detail.intersections[0].point);
             this.hasTeleportPoint = true;
+            var markerPoint = intersectionPoint;
+            markerPoint.y = markerPoint.y + 0.8; // TODO: find better way to set character height
+            //console.log("in raycaster-intersection, components: "+this.components);
+            //console.log("in raycaster-intersection, teleporter: "+this.components['teleporter']);
+            var teleporterComponent = this.components['teleporter'];
+            if (teleporterComponent.isRaycasterActive) {
+                var marker = teleporterComponent.data.marker;
+                marker.setAttribute("position", AFRAME.utils.coordinates.stringify(markerPoint));
+                marker.setAttribute("visible", "true");
+            }
         });
         this.el.addEventListener('raycaster-intersection-cleared', function (evt) {
-            console.log('Raycast intersection cleared!');
+           // console.log('Raycast intersection cleared!');
             this.hasTeleportPoint = false;
+            var teleporterComponent = this.components['teleporter'];
+            var marker = teleporterComponent.data.marker;
+            marker.setAttribute("visible", "false");
         });
     },
     showRaycaster: function() {
@@ -41,6 +56,7 @@ AFRAME.registerComponent('teleporter', {
             var teleportPoint = AFRAME.utils.coordinates.parse(this.el.teleportPoint);
             teleportPoint.y = teleportPoint.y + 2; // TODO: find better way to set character height
             character.setAttribute("position", AFRAME.utils.coordinates.stringify(teleportPoint));
+            this.data.marker.setAttribute("visible", "false");
         }
         this.isRaycasterActive = false;
         this.el.setAttribute("material", "color: cyan; shader: flat");
@@ -49,23 +65,10 @@ AFRAME.registerComponent('teleporter', {
         console.log("in showRaycaster");
         this.isRaycasterActive = false;
         this.el.setAttribute("material", "color: cyan; shader: flat");
+        this.data.marker.setAttribute("visible", "false");
     },
     play: function() {
         console.log("in teleporter play");
-    },
-    tick: function(time, timeDelta) {
     }
 });
 
-AFRAME.registerComponent('teleport-target', {
-
-    init: function () {
-        console.log("in teleporter-target init");
-    },
-    play: function() {
-        console.log("in teleporter-target play");
-    },
-    tick: function(time, timeDelta) {
-    }
-
-});
